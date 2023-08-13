52d1d85250ceb83945c467f6c3568abff3fa3a8f
tianheg <yidajiabei@gmail.com>
2023-08-13 10:59:12 +0800

    v1.1.5

--- scripts/data.js
+++ scripts/data.js
@@ -1,11 +1,13 @@
 import fs from 'node:fs'
-import rl from 'readline'
+import rl from 'node:readline'
 import got from 'got'
 
-let allDocUrls = ''
-
+let webDocUrls
 const SITEMAP_URL =
   'https://developer.mozilla.org/sitemaps/en-us/sitemap.xml.gz'
+const WEB_PATH = 'https://developer.mozilla.org/en-US/docs/Web'
+
+const onlyAllowWebUrls = (url) => url.startsWith(WEB_PATH)
 
 /**
  * Get all MDN Web Documentation URLs
@@ -24,13 +26,16 @@ const getDocUrls = async () => {
     },
   })
   const sitemap = body.toString()
+  const allDocUrls = []
 
   let match
   while ((match = SITEMAP_URL_REGEX.exec(sitemap))) {
-    allDocUrls = allDocUrls.concat(match[1] + ',')
+    allDocUrls.push(match[1])
   }
 
-  return allDocUrls
+  webDocUrls = allDocUrls.filter(onlyAllowWebUrls);
+
+  return webDocUrls
 }
 
 await getDocUrls()
@@ -39,7 +44,7 @@ await getDocUrls()
 const readInterface = rl.createInterface({
   input: fs.createReadStream('./randomNewTab.js'),
   output: process.stdout,
-  console: false,
+  terminal: false
 })
 
 const lines = []
@@ -47,6 +52,6 @@ const lines = []
 readInterface.on('line', (line) => lines.push(line))
 
 readInterface.on('close', () => {
-  lines.splice(0, 1, `const MDN = [${allDocUrls.slice(0, -1).split(',').map((url) => `'${url}'`).join(', ')}]`)
+  lines.splice(0, 1, `const MDN = [${webDocUrls.map((url) => `'${url}'`).join(', ')}]`)
   fs.writeFileSync('./randomNewTab.js', lines.join('\n'))
 })
